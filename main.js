4732625d530a1fda21b47c3c6fe4bf89f530ae60
macsteve4 <71797763+macsteve4@users.noreply.github.com>
2021-03-02 14:02:46 -0500

    Misc. Build Overhaul
    Added calendar with dates for the zodiac signs, the complete button skeletons, as well as fonts and icon improvements.

--- main.js
+++ main.js
@@ -1,23 +1,82 @@
+function fade2Content() {
+	console.log('fade2Content called');
+	document.body.classList.remove("blackScreen");
+  }
 
-//change port to test first time load
+const subaruobj = document.getElementById('subaru');
+const vwobj = document.getElementById('vw');
+const miataobj = document.getElementById('miata');
+const hondaobj = document.getElementById('honda');
+const jagobj = document.getElementById('jag');
+const toyotaobj = document.getElementById('toyota');
+const audiobj = document.getElementById('audi');
+const fordobj = document.getElementById('ford');
+const jeepobj = document.getElementById('jeep');
+const volvoobj = document.getElementById('volvo');
+const mercobj = document.getElementById('merc');
+const lincolnobj = document.getElementById('lincoln');
 
-var spectralKitten;
+//make 11 copies of these with the other cars \/
+audiobj.addEventListener('click', function(){
+	console.log('User clicked Audi!');
+	UserPicked('audi');
+});
 
-var init = function(){	
-	spectralKitten = new SpectralKitten();
-	
-	spectralKitten.loadCards(
-		function(cards)
-		{
-			console.log("Cards Loaded : " + spectralKitten.cards.length);
+function UserPicked(whichOne){
+	console.log('User picked ' + whichOne);
+};
+//make 11 copies of these with the other cars ^
 
-			var source = $("#card-list-template").html();
-			var template = Handlebars.compile(source);
+const userSubmitObj = document.getElementById('userSubmit');
 
-			var context = {cards:spectralKitten.cards};
-			var html = template(context);
-			$("#card-list").html(html);
+userSubmitObj.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  // Get the user first name and store it in a variable
+  const userFirstName = document.getElementById('fname').value;
+  console.log('userFirstName is: ' + userFirstName);
 
-		}
-	);
-}
+  // Get the user date and extract the month of year and day of month
+  const userBday = new Date(document.getElementById('bday').value);
+  console.log('userBday is: ' + userBday);
+
+  // Figure out the zodiac sign based on month and of year and day of month
+  let whichMonth = userBday.getUTCMonth();
+  whichMonth++;
+  console.log('whichMonth is: ' + whichMonth);
+
+  const whichDayOfMonth = userBday.getUTCDate();
+  console.log('whichDayOfMonth is: ' + whichDayOfMonth);
+
+  let AstroSign = "";
+
+  if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+    AstroSign = "cap";
+  } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+    AstroSign = "sag";
+  } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+    AstroSign = "sco";
+  } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+    AstroSign = "lib";
+  } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+    AstroSign = "vir";
+  } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+    AstroSign = "leo";
+  } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+    AstroSign = "can";
+  } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+    AstroSign = "gem";
+  } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+    AstroSign = "tau";
+  } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+    AstroSign = "ari";
+  } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+    AstroSign = "pis";
+  } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+    AstroSign = "aqu";
+  }
+
+  console.log('AstroSign is: ' + AstroSign);
+
+  userPicked(AstroSign);
+
+}); 
