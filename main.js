f4690ab5a83f02a4292b49d7892b5b48fa917abc
macsteve4 <71797763+macsteve4@users.noreply.github.com>
2021-03-02 22:50:43 -0500

    UI Rebuild and Internal Organization
    Added resized buttons and icons, as well as revamped the internal structure of the crucial resource files.

--- main.js
+++ main.js
@@ -14,18 +14,162 @@ const fordobj = document.getElementById('ford');
 const jeepobj = document.getElementById('jeep');
 const volvoobj = document.getElementById('volvo');
 const mercobj = document.getElementById('merc');
-const lincolnobj = document.getElementById('lincoln');
+const marquisobj = document.getElementById('marquis');
 
-//make 11 copies of these with the other cars \/
-audiobj.addEventListener('click', function(){
-	console.log('User clicked Audi!');
-	UserPicked('audi');
+const displayObj = document.getElementById('display');
+
+subaruobj.addEventListener('click', function (beej) {
+  console.log('subaru clicked: ' + beej.target.id);
+  const idName = beej.target.id;
+  userPicked(idName);
 });
 
-function UserPicked(whichOne){
+vwobj.addEventListener('click', function (beej) {
+  console.log('vw clicked: ' + beej.target.id); 
+  const idName = beej.target.id;
+  userPicked(idName);
+});
+
+miataobj.addEventListener('click', function (beej) {
+  console.log('miata clicked: ' + beej.target.id);
+  const idName = beej.target.id;
+  userPicked(idName);
+});
+
+marquisobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+audiobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+jeepobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+mercobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+hondaobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+toyotaobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+jagobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+fordobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+volvoobj.addEventListener('click', function (beej) {
+	console.log('audi clicked: ' + beej.target.id);
+	const idName = beej.target.id;
+	userPicked(idName);
+});
+
+function userPicked(whichOne) {
 	console.log('User picked ' + whichOne);
-};
-//make 11 copies of these with the other cars ^
+	switch (whichOne) {
+		case 'subaru':
+		  	displayObj.src = 'images/wrx.png';
+		  	// display date range
+		  	// display description
+		  	// play sound
+		  	break;
+		case 'vw':
+		  	displayObj.src = 'images/vw.png';
+		   	// display date range
+		  	// display description
+		  	// play sound
+		  	break;  
+		case 'miata':
+		  	displayObj.src = 'images/miata.png';
+		  	// display date range
+		  	// display description
+		 	// play sound
+		  	break;
+		case 'audi':
+			displayObj.src = 'images/audi.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'jag':
+			displayObj.src = 'images/jag.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'jeep':
+			displayObj.src = 'images/jeep.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'marquis':
+			displayObj.src = 'images/marquis.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'toyota':
+			displayObj.src = 'images/toyota.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'volvo':
+			displayObj.src = 'images/volvo.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'merc':
+			displayObj.src = 'images/merc.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'ford':
+			displayObj.src = 'images/ford.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+		case 'honda':
+			displayObj.src = 'images/honda.png';
+			// display date range
+			// display description
+			// play sound
+			break;
+
+		default:
+		  displayObj.src = 'images/car_stage.png';
+	  }
+	}
 
 const userSubmitObj = document.getElementById('userSubmit');
 
@@ -50,29 +194,29 @@ userSubmitObj.addEventListener('click', function () {
   let AstroSign = "";
 
   if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
-    AstroSign = "cap";
+    AstroSign = "volvo";
   } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
-    AstroSign = "sag";
+    AstroSign = "jeep";
   } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
-    AstroSign = "sco";
+    AstroSign = "ford";
   } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
-    AstroSign = "lib";
+    AstroSign = "audi";
   } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
-    AstroSign = "vir";
+    AstroSign = "toyota";
   } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
-    AstroSign = "leo";
+    AstroSign = "jag";
   } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
-    AstroSign = "can";
+    AstroSign = "honda";
   } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
-    AstroSign = "gem";
+    AstroSign = "miata";
   } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
-    AstroSign = "tau";
+    AstroSign = "vw";
   } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
-    AstroSign = "ari";
+    AstroSign = "subaru";
   } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
-    AstroSign = "pis";
+    AstroSign = "marquis";
   } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
-    AstroSign = "aqu";
+    AstroSign = "merc";
   }
 
   console.log('AstroSign is: ' + AstroSign);
